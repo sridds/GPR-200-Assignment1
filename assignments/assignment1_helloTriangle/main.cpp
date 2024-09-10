bf898fae6fd5cd649de35a30cf7a093b5acac2e0
Seth <seth.riddensdale@mymail.champlain.edu>
2024-09-10 12:26:55 -0400

    Finished triangle code. Pre-split

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -11,29 +11,36 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f,
-	-0.5f, -0.5f, 0.0f,
+	// positions          // colors
+	-0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f, // BOTTOM RIGHT
+	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, // BOTTOM LEFT
+	 0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f  // TOP
+	//-0.5f, -0.5f, 0.0f,
 };
-unsigned int indices[] = {  // note that we start from 0!
+unsigned int indices[] = {
 	0, 1, 3,  // first Triangle
 	1, 2, 3   // second Triangle
 };
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"uniform float time;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos.x, aPos.y + (sin(time * 2.0f + aPos.x) / 5.0f), aPos.z, 1.0);\n"
+"   ourColor = aColor;\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"uniform float value;\n"
 "void main()\n"
 "{\n"
-"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"};\0";
+"   FragColor = vec4(ourColor * value, 1.0);\n"
+"}\n\0";
 
 int main() {
 	printf("Initializing...");
@@ -108,18 +115,26 @@ int main() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// draw our first triangle
+	glUseProgram(shaderProgram);
 
 	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
 	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
 	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-	glBindVertexArray(0);
+	glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 
 	//Render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -129,15 +144,29 @@ int main() {
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT); // clears the color buffer
 
-		// draw our first triangle
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		//glDrawArrays(GL_TRIANGLES, 0, 6);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		// update shader uniform
+		double  timeValue = glfwGetTime();
+		float colorValue = static_cast<float>(sin(timeValue) / 2.0 + 0.5);
+
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "value");
+		int vertexTimeLocation = glGetUniformLocation(shaderProgram, "time");
+
+		glUniform1f(vertexColorLocation, colorValue);
+		glUniform1f(vertexTimeLocation, timeValue);
+
+		// render the triangle
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		// glBindVertexArray(0); // no need to unbind it every time 
 
 		//Drawing happens here!
 		glfwSwapBuffers(window);
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
 	printf("Shutting down...");
 }
