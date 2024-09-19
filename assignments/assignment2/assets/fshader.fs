5bd11f938455f835fddf95177c112e47644810cb
Seth <seth.riddensdale@mymail.champlain.edu>
2024-09-19 13:19:27 -0400

    Followed the quad tutorial and got indices working for Assignment 2

--- assignments/assignment2/assets/fshader.fs
+++ assignments/assignment2/assets/fshader.fs
@@ -1,12 +1,6 @@
 #version 330 core
 out vec4 FragColor;
-in vec3 ourColor;
-in vec2 TexCoord;
-
-uniform float value;
-uniform sampler2D texture1;
-
 void main()
 {
-   FragColor = mix(texture(texture1, TexCoord), vec4(ourColor * value, 1.0), 0.5);
+   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 };
--- assignments/assignment2/main.cpp
+++ assignments/assignment2/main.cpp
@@ -12,16 +12,13 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-// this is the input vertex data for OpenGL. these are in normalized device coordinates
 float vertices[] = {
-	// positions           // colors			// Texture coords
-	-0.5f, 0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	1.0f, 0.1f,		// BOTTOM RIGHT
-	 0.5f, 0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	-0.2f, 0.1f,		    // BOTTOM LEFT
-	 0.0f,  -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,	-0.5f, 1.0f		    // TOP
+	 0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
 };
-
-// for the quad (next assignment)
-unsigned int indices[] = {
+unsigned int indices[] = {  // note that we start from 0!
 	0, 1, 3,  // first Triangle
 	1, 2, 3   // second Triangle
 };
@@ -33,7 +30,7 @@ int main() {
 		return 1;
 	}
 	// Create a window
-	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Triangle Solution", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Assignment 2", NULL, NULL);
 	if (window == NULL) {
 		printf("GLFW failed to create window");
 		return 1;
@@ -47,87 +44,38 @@ int main() {
 	// build and compile shader program
 	Shader ourShader("assets/vshader.vs", "assets/fshader.fs");
 
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO); // Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 
 	// Copy our vertices array in a buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// Set the vertex attributes pointers
-	// -- Position attribute -- 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// -- Color attribute --
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
-	// -- Tex Coord Attribute --
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-
-	// load and create a texture
-	unsigned int texture1;
-
-	glGenTextures(1, &texture1);
-	glBindTexture(GL_TEXTURE_2D, texture1);
-	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// set filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	// load image, create texture, and generate minmaps
-	int width, height, nrChannels;
-	stbi_set_flip_vertically_on_load(false); // tell stb_image.h to flip loaded texture's on the y-axis.
-	unsigned char* data = stbi_load("assets/IMG_0736.jpg", &width, &height, &nrChannels, 0);
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-	stbi_image_free(data);
-
-	ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
-	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
-
-	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
-	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-	// glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	// Render loop
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 
-		//Clear framebuffer
-		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT); // clears the color buffer
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-		// update shader uniform
-		double  timeValue = glfwGetTime();
-		float colorValue = static_cast<float>(sin(timeValue) / 2.0 + 0.5);
-
-		// bind textures on corresponding texture units
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture1);
-
-		// Get the locations of the following variables
-		ourShader.setFloat("value", colorValue);
-		ourShader.setFloat("time", timeValue);
-
-		// Render the triangle
+		// draw our first triangle
 		ourShader.use();
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		//Drawing happens here!
 		glfwSwapBuffers(window);
@@ -135,6 +83,7 @@ int main() {
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	printf("Shutting down...");
 }
