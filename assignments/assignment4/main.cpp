c354fa765c253bfe4eef791b8664eb25917dc5fc
Seth <seth.riddensdale@mymail.champlain.edu>
2024-10-16 15:09:40 -0400

    Adjustments to fit assignment critera - Ensured far clip plane is set to 1000 - Created 20 cubes - Randomized position of cubes

--- assignments/assignment4/main.cpp
+++ assignments/assignment4/main.cpp
@@ -69,7 +69,7 @@ float vertices[] = {
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 25));
 float lastX = SCREEN_WIDTH / 2.0f;
 float lastY = SCREEN_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -77,18 +77,30 @@ bool firstMouse = true;
 float deltaTime = 0.0f; // Time between current frame and last frame
 float lastFrame = 0.0f; // Time of last frame
 
+float minRange = -8.0f;
+float maxRange = 8.0f;
 // world space positions of our cubes
 glm::vec3 cubePositions[] = {
-	glm::vec3(0.0f,  0.0f,  0.0f),
-	glm::vec3(2.0f,  5.0f, -15.0f),
-	glm::vec3(-1.5f, -2.2f, -2.5f),
-	glm::vec3(-3.8f, -2.0f, -12.3f),
-	glm::vec3(2.4f, -0.4f, -3.5f),
-	glm::vec3(-1.7f,  3.0f, -7.5f),
-	glm::vec3(1.3f, -2.0f, -2.5f),
-	glm::vec3(1.5f,  2.0f, -2.5f),
-	glm::vec3(1.5f,  0.2f, -1.5f),
-	glm::vec3(-1.3f,  1.0f, -1.5f)
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange)),
+	glm::vec3(ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange), ew::RandomRange(minRange, maxRange))
 };
 
 int main() {
@@ -178,7 +190,7 @@ int main() {
 		ourShader.use();
 
 		// pass projection matrix to shader (note that in this case it could change every frame)
-		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 1000.0f);
 		ourShader.setMat4("projection", projection);
 
 		// create transformations
@@ -192,7 +204,7 @@ int main() {
 		glBindVertexArray(VAO);
 
 		// creates a bunch of cubes
-		for (unsigned int i = 0; i < 10; i++) {
+		for (unsigned int i = 0; i < 20; i++) {
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, cubePositions[i]);
 			float angle = (20.0f * i) + (timeValue * 25.0f);
